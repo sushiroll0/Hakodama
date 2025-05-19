3c44c0af7443eede5efdacf9992c0be86147d7fc
sushiroll0 <lcoss02@jaguar.tamu.edu>
2025-05-18 20:46:04 -0500

    Update hamburger menu for  script.js

--- script.js
+++ script.js
@@ -203,3 +203,19 @@ if (searchInput) {
     loadPosts(null, searchValue);
   });
 }
+
+
+
+
+// Hamburger Menu Toggle
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.getElementById("hamburger");
+  const navMenu = document.getElementById("nav-menu");
+
+  if (hamburger && navMenu) {
+    hamburger.addEventListener("click", () => {
+      navMenu.classList.toggle("show");
+    });
+  }
+});
+
